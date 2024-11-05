5dfd66799267ab381618ded938380af4bf3a46b5
CARBONNEAUX Mathieu <mcarbonneaux@users.noreply.github.com>
2024-11-05 21:56:56 +0000

    utilisation de zbx_cfg_line_t

--- zbx_php.c
+++ zbx_php.c
@@ -118,7 +118,7 @@ int load_php_env_config(void)  {
 	    { NULL },
     };
 	#else
-    static zbx_addr_t cfg[] = {
+    static zbx_cfg_line_t cfg[] = {
 	    { "PHP_SCRIPT_PATH", &php_path, ZBX_CFG_TYPE_STRING , ZBX_CONF_PARM_MAND, 0, 0 },
 	    { NULL },
     };
